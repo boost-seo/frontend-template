dea8d4e7cee82e05352cdc25130dcdc2d56d118e
Gregor Žunič <36313686+gregpr07@users.noreply.github.com>
2023-10-10 14:01:04 +0200

    make setup easier

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,6 +6,6 @@ export const isLocal = process.env.NODE_ENV === 'development';
 export const environmentVariables = {
   API_BACKEND_URL: isProd
     ? constants.backendUrl
-    : process.env.API_BACKEND_URL?.replace(/\/$/, '') || '',
+    : process.env.API_BACKEND_URL?.replace(/\/$/, '') || constants.backendUrl,
   API_KEY: process.env.API_KEY || '',
 };
